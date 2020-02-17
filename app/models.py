092a66d75365ffb23f6cf0beab08a127cf0a4604
Vishwajeet Mishra <vishwajeet.mishra@hashedin.com>
2020-02-18 01:08:45 +0530

    Commit with Milestone 1
    HTML Template with curd and list of customer also

--- app/models.py
+++ app/models.py
@@ -3,6 +3,13 @@ class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(64),unique=True)
     email=db.Column(db.String(120),unique=True)
-    __tablename='CustomerUser'
+    __tablename='customerUser'
     def __repr__(self):
         return '<User {}>'.format(self.username)
+class Temp(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+    html=db.Column(db.String(1000),unique=True)
+    __tablename='template'
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
