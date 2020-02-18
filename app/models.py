07c02e55b54d5dec6d0a2bd6dc0f3098b05965fd
Vishwajeet Mishra <vishwajeet.mishra@hashedin.com>
2020-02-18 23:52:24 +0530

    Commit with Milestone 3
    CSV to db all 5 succesfully

--- app/models.py
+++ app/models.py
@@ -18,6 +18,57 @@ class Dynmic_Temp(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
     html=db.Column(db.String(1000),unique=True)
-    __tablename='template'
+    __tablename='dynamic_template'
     def __repr__(self):
         return '<User {}>'.format(self.name)
+class Order(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    purchaseDate=db.Column(db.DateTime)
+    totalPrice=db.Column(db.Integer)
+    __tablename='order'
+    def __repr__(self):
+        return '<Order on {}>'.format(self.purchaseDate)
+class Reviews(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    userProductid=db.Column(db.Integer)
+    productRating=db.Column(db.Integer)
+    reviewTitle = db.Column(db.String(100))
+    reviewDetails=db.Column(db.String(1000))
+    __tablename='reviews'
+    def __repr__(self):
+        return '<Reviews on {}>'.format(self.userProductid)
+class UserProductList(db.Model):
+    Id=db.Column(db.Integer,primary_key=True)
+    userId=db.Column(db.Integer)
+    productId=db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
+    orderId=db.Column(db.Integer)
+    __tablename='userprod'
+    def __repr__(self):
+        return '<UserProductList on {}>'.format(self.userid)
+class Product(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    SKU=db.Column(db.String(20))
+    productName=db.Column(db.String(20))
+    brand = db.Column(db.String(100))
+    productDescription=db.Column(db.String(1000))
+    color = db.Column(db.String(20))
+    unitPrice = db.Column(db.Integer)
+    __tablename='product'
+    def __repr__(self):
+        return '<Product on {}>'.format(self.productName)
+class Customers(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    first_name=db.Column(db.String(100))
+    last_name=db.Column(db.String(100))
+    gender = db.Column(db.String(100))
+    email=db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    address = db.Column(db.String(1000))
+    state = db.Column(db.String(100))
+    zipcode = db.Column(db.Integer)
+    phoneNumber = db.Column(db.String(20))
+    registrationDate = db.Column(db.DateTime)
+    __tablename='customers'
+    def __repr__(self):
+        return '<customers on {}>'.format(self.first_name)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+from datetime import date, datetime
+
 from flask_mail import Message
 from flask import request,jsonify
 from app import app, db, mail
-from app.models import User,Temp,Dynmic_Temp
+from app.models import User,Temp,Dynmic_Temp,Order,Reviews,UserProductList,Product,Customers
+import csv
 
 @app.route('/hello-world')
 def hello_world():
@@ -131,3 +134,108 @@ def email_dynamic():
         msg=Message('Hello',sender='yourId@gmail.com',recipients=list(i.email),html=temp.html.format(i.username,i.price,couponCode))
         mail.send(msg)
     return "Sent"
+
+@app.route("/order",methods=['GET'])
+def order():
+    result=[]
+    with open('Order.csv') as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        n = len(next(csvReader))
+        csvDataFile.seek(0)
+        for i in range(0, n):
+            result.append([])
+        for row in csvReader:
+            for x in range(0, n):
+                result[x].append(row[x])
+    for i in range(1,len(result[0])):
+        a,b,c=result[1][i].split('/')
+        user = Order(id=int(result[0][i]), purchaseDate=date(int(c),int(b),int(a)),totalPrice=int(result[2][i]))
+        db.session.add(user)
+    db.session.commit()
+    return "Transefer to db"
+
+@app.route("/reviews",methods=['GET'])
+def reviews():
+    result=[]
+    with open('Reviews.csv') as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        n = len(next(csvReader))
+        csvDataFile.seek(0)
+        for i in range(0, n):
+            result.append([])
+        for row in csvReader:
+            for x in range(0, n):
+                result[x].append(row[x])
+    for i in range(1,len(result[0])):
+        user = Reviews(id=int(result[0][i]), userProductid=int(result[1][i]),productRating=int(result[2][i]),reviewTitle=int(result[3][i]),reviewDetails=int(result[4][i]))
+        db.session.add(user)
+    db.session.commit()
+    return "Transefer to db"
+
+@app.route("/userprod",methods=['GET'])
+def userprod():
+    result=[]
+    with open('UserProductList.csv') as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        n = len(next(csvReader))
+        csvDataFile.seek(0)
+        for i in range(0, n):
+            result.append([])
+        for row in csvReader:
+            for x in range(0, n):
+                result[x].append(row[x])
+    for i in range(1,len(result[0])):
+        user = UserProductList(Id=int(result[0][i]), userId=int(result[1][i]),productId=int(result[2][i]),quantity=int(result[3][i]),orderId=int(result[4][i]))
+        db.session.add(user)
+    db.session.commit()
+    return "Transefer to db"
+
+@app.route("/product",methods=['GET'])
+def product():
+    result=[]
+    with open('Product.csv') as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        n = len(next(csvReader))
+        csvDataFile.seek(0)
+        for i in range(0, n):
+            result.append([])
+        for row in csvReader:
+            for x in range(0, n):
+                result[x].append(row[x])
+    for i in range(1,len(result[0])):
+        user = Product(id=int(result[0][i]), SKU=result[1][i],productName=result[2][i],brand=result[3][i],productDescription=result[4][i],color=result[5][i],unitPrice=int(result[6][i]))
+        db.session.add(user)
+    db.session.commit()
+    return "Transefer to db"
+
+@app.route("/customers",methods=['GET'])
+def customers():
+    result=[]
+    with open('Customers.csv') as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        n = len(next(csvReader))
+        csvDataFile.seek(0)
+        for i in range(0, n):
+            result.append([])
+        for row in csvReader:
+            for x in range(0, n):
+                result[x].append(row[x])
+    for i in range(1,len(result[0])):
+        user = Customers(id=int(result[0][i]), first_name=result[1][i],last_name=result[2][i],gender=result[3][i],email=result[4][i],age=int(result[5][i]),address=result[6][i],state=result[7][i],zipcode=int(result[8][i]),phoneNumber=result[9][i],registrationDate=datetime.strptime(result[10][i],'%Y-%m-%dT%H:%M:%Sz'))
+        db.session.add(user)
+    db.session.commit()
+    return "Transefer to db"
+
+# @app.route("/a",methods=['GET','DELETE'])
+# def g():
+#     id=request.args.get('id')
+#     if request.method=='GET':
+#         cols = ['id', 'first_name', 'last_name','gender','email','age','address','state','zipcode','phoneNumber','registrationDate']
+#         data = Customers.query.all()
+#         result = [{col: getattr(d, col) for col in cols} for d in data]
+#         return jsonify(result=result)
+#     if request.method=='DELETE':
+#         Customers.query.filter_by(id=id).delete()
+#         db.session.commit()
+#         return 'success deletion with id {}'.format(id)
+
